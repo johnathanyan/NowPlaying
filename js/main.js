561bd627a61865edaf6e39449190553dddb4c3b2
Johnathan Yan <johnathanyan@gmail.com>
2016-11-18 19:09:33 -0500

    added setting to change username
    now everyone can use the extension by providing their last.fm username.

--- js/main.js
+++ js/main.js
@@ -5,6 +5,8 @@ $(window).load(function() {
     var apiKey = '07e20f0551b8325642d01a468d6c9e2b';
     var apiSecret = 'f0e2309ac1f113b331ebb8ba7197b1bc';
 
+    var storage = chrome.storage.local;
+
     // create a Cache object
     var cache = new LastFMCache();
 
@@ -15,71 +17,86 @@ $(window).load(function() {
         cache: cache
     });
 
-    // get most recent song
-    lastfm.user.getRecentTracks({ limit: 1, user: 'johnathanyan' }, {
-        success: function(data) {
+    function save_options() {
+        storage.set({ "_userName": document.getElementById('user').value });
+    }
 
-            /* render album art and info */
-            lastfm.album.getInfo({ artist: data.recenttracks.track["0"].artist["#text"], album: data.recenttracks.track["0"].album["#text"] }, {
-                success: function(_data) {
-                    // Track name and artist
-                    document.getElementById('Header').innerHTML = '<h1><center><font color="white">Now Playing:</font></center></h1>';
-                    document.getElementById('Playing').innerHTML = '<center><marquee behavior="alternate" vspace=6 direction="left"><font size=4> <b>' +
-                        data.recenttracks.track["0"].name + '</b></marquee><i>' + data.recenttracks.track["0"].artist["#text"] + '</i></center>';
+    document.getElementById('save').addEventListener('click',
+        save_options);
 
-                    // Fill in missing info
-                    if (typeof _data.album.tags.tag["0"] === "undefined") {
-                        _data.album.tags.tag["0"] = "Unknown";
-                    }
-                    if (typeof _data.album.tags.tag["1"] === "undefined") {
-                        _data.album.tags.tag["1"] = "Unknown";
-                    }
+    storage.get("_userName",
+        function(result) {
+            document.getElementById('user').value = result._userName
 
-                    // Album Info
-                    document.getElementById('AlbumArt').innerHTML = '<center><img src=' + _data.album.image[3]["#text"] + '>' +
-                        '<br><br><h2>Album Info:</h2><br><b>Name:</b> ' + _data.album.name + '<br><b>Genre or Info:</b> ' + _data.album.tags.tag["0"].name + ' / ' +
-                        _data.album.tags.tag["1"].name + '<br><b>Plays:</b> ' + _data.album.playcount + '<br><br></center>';
+            var userName = document.getElementById('user').value;
+            console.log(userName);
 
-                    // Album Summary
-                    if (typeof _data.album.wiki === "undefined") {
-                        document.getElementById('Summary').innerHTML = '<hr>';
-                    } else {
-                        document.getElementById('Summary').innerHTML = '<center><b>Summary:</b><br>' + _data.album.wiki.summary + '<br><br><hr>';
-                    }
+            // get most recent song
+            lastfm.user.getRecentTracks({ limit: 1, user: userName }, {
+                success: function(data) {
+
+                    /* render album art and info */
+                    lastfm.album.getInfo({ artist: data.recenttracks.track["0"].artist["#text"], album: data.recenttracks.track["0"].album["#text"] }, {
+                        success: function(_data) {
+                            // Track name and artist
+                            document.getElementById('Header').innerHTML = '<h1><center><font color="white">Now Playing:</font></center></h1>';
+                            document.getElementById('Playing').innerHTML = '<center><marquee behavior="alternate" vspace=6 direction="left"><font size=4> <b>' +
+                                data.recenttracks.track["0"].name + '</b></marquee><i>' + data.recenttracks.track["0"].artist["#text"] + '</i></center>';
+
+                            // Fill in missing info
+                            if (typeof _data.album.tags.tag["0"] === "undefined") {
+                                _data.album.tags.tag["0"] = "Unknown";
+                            }
+                            if (typeof _data.album.tags.tag["1"] === "undefined") {
+                                _data.album.tags.tag["1"] = "Unknown";
+                            }
+
+                            // Album Info
+                            document.getElementById('AlbumArt').innerHTML = '<center><img src=' + _data.album.image[3]["#text"] + '>' +
+                                '<br><br><h2>Album Info:</h2><br><b>Name:</b> ' + _data.album.name + '<br><b>Genre or Info:</b> ' + _data.album.tags.tag["0"].name + ' / ' +
+                                _data.album.tags.tag["1"].name + '<br><b>Plays:</b> ' + _data.album.playcount + '<br><br></center>';
+
+                            // Album Summary
+                            if (typeof _data.album.wiki === "undefined") {
+                                document.getElementById('Summary').innerHTML = '<hr>';
+                            } else {
+                                document.getElementById('Summary').innerHTML = '<center><b>Summary:</b><br>' + _data.album.wiki.summary + '<br><br><hr>';
+                            }
+                        }
+                    });
+
+                    // get lyrics using musixmatch
+                    $.getJSON("http://api.musixmatch.com/ws/1.1/matcher.track.get?apikey=105305da3eb9047cf95bcd5d2c38672a&q_track=" +
+                        data.recenttracks.track[0].name + "&q_artist=" +
+                        data.recenttracks.track["0"].artist["#text"],
+                        function(data) {
+                            // url of lyrics
+                            var js = data.message.body.track.track_share_url;
+                            var xhr = new XMLHttpRequest();
+                            xhr.open('GET', js, true);
+                            xhr.responseType = 'document';
+                            xhr.send();
+                            xhr.onload = function(e) {
+                                var doc = e.target.responseXML;
+                                var lyrics = doc.getElementsByClassName("mxm-lyrics__content")[0].innerHTML + "<br>" +
+                                    doc.getElementsByClassName("mxm-lyrics__content")[1].innerHTML;
+                                lyrics = lyrics.replace(/\n/g, "<br>")
+                                document.getElementById('Lyrics').innerHTML = '<br><center><h2>Lyrics:</h2><br>' + lyrics + '<br><br>';
+
+                            }
+
+                            var lyrics;
+
+                            /* use api to get lyrics here - MUST HAVE LICENSE TO USE API
+                            ------------------
+                            $.getJSON("http://api.musixmatch.com/ws/1.1/track.lyrics.get?apikey=105305da3eb9047cf95bcd5d2c38672a&track_id=" + js, function(data) {
+                                lyrics = data.message.body.lyrics.lyrics_body;
+
+                                document.getElementById('top_artist').innerHTML = lyrics;
+                            });*/
+
+                        });
                 }
             });
-
-            // get lyrics using musixmatch
-            $.getJSON("http://api.musixmatch.com/ws/1.1/matcher.track.get?apikey=105305da3eb9047cf95bcd5d2c38672a&q_track=" +
-                data.recenttracks.track[0].name + "&q_artist=" +
-                data.recenttracks.track["0"].artist["#text"],
-                function(data) {
-                    // url of lyrics
-                    var js = data.message.body.track.track_share_url;
-                    var xhr = new XMLHttpRequest();
-                    xhr.open('GET', js, true);
-                    xhr.responseType = 'document';
-                    xhr.send();
-                    xhr.onload = function(e) {
-                        var doc = e.target.responseXML;
-                        var lyrics = doc.getElementsByClassName("mxm-lyrics__content")[0].innerHTML + "<br>" +
-                            doc.getElementsByClassName("mxm-lyrics__content")[1].innerHTML;
-                        lyrics = lyrics.replace(/\n/g, "<br>")
-                        document.getElementById('Lyrics').innerHTML = '<br><center><h2>Lyrics:</h2><br>' + lyrics;
-
-                    }
-
-                    var lyrics;
-
-                    /* use api to get lyrics here - MUST HAVE LICENSE TO USE API
-                    ------------------
-                    $.getJSON("http://api.musixmatch.com/ws/1.1/track.lyrics.get?apikey=105305da3eb9047cf95bcd5d2c38672a&track_id=" + js, function(data) {
-                        lyrics = data.message.body.lyrics.lyrics_body;
-
-                        document.getElementById('top_artist').innerHTML = lyrics;
-                    });*/
-
-                });
-        }
-    });
+        });
 });
